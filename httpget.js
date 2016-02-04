0f9653b08aded205afffde21592b90aee4a7c3d2
lordmg <marianbergerot@gmail.com>
2016-02-04 20:01:42 +0100

    first commit

---
+++ httpget.js
@@ -0,0 +1,22 @@
+function httpGet(url) {
+    return new Promise(
+        function (resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (this.status === 200) {
+                    // Success
+                    resolve(this.response);
+                } else {
+                    // Something went wrong (404 etc.)
+                    reject(new Error(this.statusText));
+                }
+            }
+            request.onerror = function () {
+                reject(new Error(
+                    'XMLHttpRequest Error: '+this.statusText));
+            };
+            request.open('GET', url);
+            request.send();
+        });
+}
+
